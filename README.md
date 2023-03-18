362f3f5b1d2ab53142d34b2d701cbd2b1f02d0f3
ynn <contact@your-diary.io>
2023-03-18 17:43:36 +0900

    Completed `cloud_functions/`.

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
+# 1. About
 
+# 2. Prerequisites
+
+# 3. `./cloud_functions/` project
+
+## 3.1 About
+
+This project creates a REST API which receives a JSON of the form `{"content": <string>}` and uploads its `content` as `<timestamp>.txt` to Cloud Storage. A signed URL (i.e. a public URL with expiration date) to access the object uploaded in Cloud Storage is returned.
+
+## 3.2 Architecture
+
+![](./readme_assets/001.png)
+
+<!-- https://docs.google.com/presentation/d/1ITQbn7CoCnWI3O8fwQH6-TurLOHx4iAe2oX_AM6DeRE/edit#slide=id.g21f1d234fe4_0_23 -->
+
+## 3.3 Usage
+
+1. Access [*IAM & Admin console*](https://console.cloud.google.com/iam-admin/iam).
+
+    1. Select `IAM` in the sidebar.
+
+    2. Click the edit icon for the principal whose value of `Name` field is `Compute Engine default service account`.
+
+    3. Select `ADD ANOTHER ROLE` to add `Service Account Token Creator` role.
+
+2. Access [*Cloud Storage console*](https://console.cloud.google.com/storage/browser) to created a bucket named `test-bucket-001-a`.
+
+3. Access [*Cloud Functions console*](https://console.cloud.google.com/functions/list) to create a function with the following settings.
+
+    - Function name: `function-1`
+
+    - Environment: `2nd gen`
+
+    - Region: `us-central1`
+
+    - Runtime: `Go 1.20`
+
+4. Upload this project to deploy a function.
+    ```bash
+    $ gcloud functions deploy function-1 --source . --gen2 --region us-central1
+    ```
+
+5. Call the API.
+
+    ```bash
+    $ curl <URL> \
+        -H "Authorization: bearer $(gcloud auth print-identity-token)" \
+        -H "Content-Type: application/json" \
+        -d '{"content": "hello"}'
+    ```
+
+    ```json
+    {
+        "status": "success",
+        "url": "https://..."
+    }
+    ```
+
+    ```bash
+    $ curl <returned URL>
+    ```
+
+## 3.4 References
 
 - [*GoogleCloudPlatform / functions-framework-go*](https://github.com/GoogleCloudPlatform/functions-framework-go)
 
--- cloud_functions/main.go
+++ cloud_functions/main.go
@@ -1,11 +1,17 @@
 package abc
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	"cloud.google.com/go/storage"
 )
 
 type Request struct {
@@ -17,9 +23,24 @@ type Response struct {
 	Content string `json:"content"`
 }
 
+const ENTRY_POINT = "http" //arbitrary string but shall match `Entry point` specified in the console
+const BUCKET_NAME = "test-bucket-001-a"
+
 func init() {
-	entryPoint := "http" //arbitrary string but shall match `Entry point` specified in the console
-	functions.HTTP(entryPoint, handler)
+	functions.HTTP(ENTRY_POINT, handler)
+}
+
+//Same as `json.MarshalIndent()` but does NOT implicitly escape HTML entities such as `<`, `>` and `&`.
+//ref: |https://stackoverflow.com/a/28596225/8776746|
+func toJSON(t any, numIndent int) ([]byte, error) {
+	buffer := &bytes.Buffer{}
+	encoder := json.NewEncoder(buffer)
+	if numIndent != 0 {
+		encoder.SetIndent("", strings.Repeat(" ", numIndent))
+	}
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(t)
+	return buffer.Bytes(), err
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -31,8 +52,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			Status:  "error",
 			Content: "failed to parse json",
 		}
-		res, _ := json.MarshalIndent(response, "", "    ")
-		w.Write([]byte(res))
+		res, _ := toJSON(response, 4)
+		w.Write(res)
 		return
 	}
 	if req.Content == "" {
@@ -41,16 +62,65 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			Status:  "error",
 			Content: "`content` field shall not be empty",
 		}
-		res, _ := json.MarshalIndent(response, "", "    ")
-		w.Write([]byte(res))
+		res, _ := toJSON(response, 4)
+		w.Write(res)
 		return
 	}
+
+	filename := fmt.Sprintf("%v.txt", time.Now().UnixMicro())
+	url, err := uploadFile(BUCKET_NAME, filename, req.Content)
+	if err != nil {
+		w.WriteHeader(500)
+		response := Response{
+			Status:  "error",
+			Content: err.Error(),
+		}
+		res, _ := toJSON(response, 4)
+		w.Write(res)
+		return
+	}
+	fmt.Printf("url: %v\n", url)
+
 	w.WriteHeader(200)
 	response := Response{
 		Status:  "success",
-		Content: fmt.Sprintf("Hello, %v!", req.Content),
+		Content: url,
 	}
-	res, _ := json.MarshalIndent(response, "", "    ")
-	w.Write([]byte(res))
+	res, _ := toJSON(response, 4)
+	fmt.Printf("res: %v\n", res)
+	fmt.Printf("res: %v\n", string(res))
+	w.Write(res)
 	return
+}
+
+func uploadFile(bucketName, filename, content string) (string, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to create a client: %v", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*60)
+	defer cancel()
+
+	bucket := client.Bucket(bucketName)
+	o := bucket.Object(filename)
+	ww := o.NewWriter(ctx)
+	if _, err = fmt.Fprintf(ww, content); err != nil {
+		return "", fmt.Errorf("failed to write to a writer: %v", err)
+	}
+	if err := ww.Close(); err != nil {
+		return "", fmt.Errorf("failed to close a writer: %v", err)
+	}
+
+	opts := storage.SignedURLOptions{
+		Method:  "GET",
+		Expires: time.Now().Add(time.Second * 30),
+	}
+	url, err := bucket.SignedURL(filename, &opts)
+	if err != nil {
+		return "", fmt.Errorf("failed to create a signed URL: %v", err)
+	}
+	return url, nil
 }
